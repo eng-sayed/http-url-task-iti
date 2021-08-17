3b216191685e1fbf7f84d88dd5b917f56e082266
eng-sayed <56679747+eng-sayed@users.noreply.github.com>
2021-08-17 19:09:12 +0200

    third

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,5 +1,13 @@
+import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 void launchURL(String url) async {
   await canLaunch(url) ? await launch(url) : throw 'Could not launch $url';
+}
+
+pushPage(BuildContext context, Widget widget) {
+  Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => widget),
+  );
 }
